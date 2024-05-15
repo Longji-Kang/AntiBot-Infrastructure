b770be3beebddadd8970b76f2380e3e84807bf63
Longji Kang <kanglongjidev@gmail.com>
2024-05-15 20:50:19 +0200

    fix: add S3 backend to terraform providers

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
-provider "aws" {
-  region = "eu-west-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket = "longji-state-bucket"
+    key = "terraform/state"
+    region = "eu-west-1"
+  }
 }
